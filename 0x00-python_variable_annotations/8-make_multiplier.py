96126e60aa5160102f12a3695c11c6e2bc94d8db
Dennis Murage <dmurage140@gmail.com>
2023-07-06 17:33:04 +0300

    Fixing Styling

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from typing import Callable
 """function that returns a function"""
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
