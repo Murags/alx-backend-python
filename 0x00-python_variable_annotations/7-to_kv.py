97a91062acd1d643a9e2bfd7ec0004d6c245fea8
Dennis Murage <dmurage140@gmail.com>
2023-07-06 17:31:02 +0300

    Fixing Styling

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Union
 """type annotation"""
+from typing import Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[float, int]) -> tuple:
     """returns a tuple"""
     return (k, v ** 2)
