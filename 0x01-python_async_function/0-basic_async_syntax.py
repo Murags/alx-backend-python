09aa43478fc1cfee42674dedf00b430b27a0a5ca
Dennis Murage <dmurage140@gmail.com>
2023-07-10 12:57:05 +0300

    Added task 0 files and README.md

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""asynchronous javascript concept
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """generates random float and wait those seconnds
+
+    Args:
+        max_delay (int): max delay value. Defaults to 10.
+
+    Returns:
+        float: number of seconds delayed
+    """
+    x = random.uniform(0, max_delay)
+    await asyncio.sleep(x)
+    return x
